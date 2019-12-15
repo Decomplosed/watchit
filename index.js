0be9f793862c04e4229324031746cfa3f22eb4ff
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-15 16:35:32 +0100

    added spawn from child_process library

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const debounce = require('lodash.debounce')
 const chokidar = require('chokidar')
 const program = require('caporal')
 const fs = require('fs')
+const { spawn } = require('child_process')
 
 program
   .version('0.0.1')
